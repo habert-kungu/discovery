3b3806a5849f382980617ec86fa4a57f71902abd
Habert kungu <habert231@gmail.com>
2024-10-08 07:36:05 +0300

    managing state and adding props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["tokyo", "nairobi", "uganda"];
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,35 @@
-function ListGroup() {
-  let items = ["tokyo", "nairobi", "ugandah"];
-  //items = [];
+import React, { useState } from "react";
 
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup(props: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>Not in items</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item "
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }} // Pass the handleClick function here
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
   );
 }
+
 export default ListGroup;
